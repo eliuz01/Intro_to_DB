b5269dc837445b8e79031e3abd367763a0bdafad
ELIUZ <osongoeliuz14@gmail.com>
2025-02-04 07:56:23 +0300

    database and tables created

---
+++ alx_book_store.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE alx_book_store; 
+SHOW DATABASES;
+USE alx_book_store;
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors (author_id)
+    );
+DESC Books;
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+DESC Authors;
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+DESC Customers;
+CREATE TABLE Orders(
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
+);
+DESC Orders;
+CREATE TABLE Order_Details(
+    orderdetail_id INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    Foreign Key (order_id) REFERENCES Orders (order_id),
+    Foreign Key (book_id) REFERENCES Books(book_id)
+);
+DESC Order_details;
