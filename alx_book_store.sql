75bda2e478ce1335a96d48cbb245d7ba9250f804
ELIUZ <osongoeliuz14@gmail.com>
2025-02-04 10:50:02 +0300

    3rd rev

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -34,7 +34,7 @@ CREATE TABLE Order_Details(
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES Orders (order_id),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
 DESC Order_details;
