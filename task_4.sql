6a71466bdb16709b6290c5e887e0f02a0bff3854
ELIUZ <osongoeliuz14@gmail.com>
2025-02-07 14:54:36 +0300

    task 4

---
+++ task_4.sql
@@ -0,0 +1,19 @@
+-- Use the alx_book_store database
+USE alx_book_store;
+
+-- Query the INFORMATION_SCHEMA to get full description of the books table
+SELECT 
+    COLUMN_NAME,
+    DATA_TYPE,
+    CHARACTER_MAXIMUM_LENGTH,
+    NUMERIC_PRECISION,
+    NUMERIC_SCALE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'  -- Specify the database name
+    AND TABLE_NAME = 'books'         -- Specify the table name
+ORDER BY 
+    ORDINAL_POSITION;                -- Order by the column position in the table
